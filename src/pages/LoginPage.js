d1694bf1d767ddd4dc1a573f0adbae5777baccfd
Yug34 <vertilax1337@gmail.com>
2021-02-25 17:34:44 +0530

    Added regex patterns for validation

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -14,14 +14,26 @@ class LoginPage extends Component {
 
         this.inputChangeHandler = this.inputChangeHandler.bind(this);
         this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+        this.signIn = this.signIn.bind(this);
     }
 
-    inputChangeHandler() {
-
+    inputChangeHandler(e) {
+        let oldState = this.state;
+        oldState.input = e.target.value;
+        this.setState(oldState);
     }
 
-    passwordChangeHandler() {
+    passwordChangeHandler(e) {
+        let oldState = this.state;
+        oldState.password = e.target.value;
+        this.setState(oldState);
+    }
 
+    signIn() {
+        let emailPattern = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        let numberPattern = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+
+        let passwordPattern = /[a-zA-Z0-9.!@#$%^&*_]{4,60}/;
     }
 
     render() {
@@ -32,8 +44,13 @@ class LoginPage extends Component {
                 </div>
                 <div>
                     Sign In
-                    <input type="text" onChange={() => this.inputChangeHandler()} defaultValue="Email or phone number"/>
-                    <input type="text" onChange={() => this.passwordChangeHandler()} defaultValue="Password"/>
+                    <form>
+                        <input type="text" onChange={(e) => this.inputChangeHandler(e)} defaultValue="Email or phone number"/>
+                        <input type="text" onChange={(e) => this.passwordChangeHandler(e)} defaultValue="Password"/>
+                        <button onClick={() => this.signIn()}>Sign in</button>
+                        <input type="checkbox" id="rememberMe" />
+                        <label htmlFor="rememberMe">Remember me</label>
+                    </form>
                 </div>
             </HeaderComponent>
         )
