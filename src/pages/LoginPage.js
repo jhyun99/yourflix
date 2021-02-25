510f25dbd1ee92a8a03032c98bbf7096fb651527
Yug34 <vertilax1337@gmail.com>
2021-02-25 14:40:05 +0530

    Added LoginPage.js

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../css/LoginPage.css';
+import styled from "styled-components";
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <HeaderComponent className="headerSection">
+            </HeaderComponent>
+        )
+    }
+}
+
+const HeaderComponent = styled.div`
+
+`;
+
+export default LoginPage;
